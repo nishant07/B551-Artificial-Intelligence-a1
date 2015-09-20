5f3d5ab2e4e5bc23efcd70882328d4f0e4099fed
Pranav Pande <pmpande@iu.edu>
2015-09-20 14:38:06 -0400

    Changing name of assignment 1 prog 2

---
+++ route.py
@@ -0,0 +1,168 @@
+import sys
+import Queue
+'''
+start_city = sys.argv[1] 
+end_city = sys.argv[2] 
+routing_option = sys.argv[3]
+routing_algo = sys.argv[4]
+'''
+f1 = open('city-gps.txt','r')
+f2 = open('road-segments.txt','r')
+f3 = open('seg_dict.txt','w')
+f4 = open('city_dict.txt','w')
+cities = []
+#t = []
+for city in f1:
+	#t.append(city.split()[0])
+	cities.append(city.split())
+
+#print t	
+#print len(cities)
+seg = []
+for segments in f2:
+	seg.append(segments.split())
+print len(seg)
+city_dict = {}
+seg_dict = {}
+for segments in seg:
+	if len(segments) != 5:
+		segments = [segments[0],segments[1],int(segments[2]),9999,segments[3]]
+	#	seg_dict.append([segments[0],segments[1],int(segments[2]),9999,segments[3]])
+	else:
+		segments = [segments[0],segments[1],int(segments[2]),int(segments[3]),segments[4]]
+	#	seg_dict.append([segments[0],segments[1],int(segments[2]),int(segments[3]),segments[4]])
+
+	if segments[0] not in seg_dict:
+		seg_dict[segments[0]] = [(segments[1],segments[2],segments[3],segments[4])]
+	else:
+		seg_dict[segments[0]].append((segments[1],segments[2],segments[3],segments[4]))
+
+
+	if segments[1] not in seg_dict:
+		seg_dict[segments[1]] = [(segments[0],segments[2],segments[3],segments[4])]
+	else:
+		seg_dict[segments[1]].append((segments[0],segments[2],segments[3],segments[4]))
+
+print len(seg_dict)
+for city in cities:
+	city_dict.update({str(city[0]) : (float(city[1]),float(city[2]))})
+print len(city_dict)
+for i,j in city_dict.items():
+	f4.write(i+str(j)+"\n")
+#print city_dict
+for i,j in seg_dict.items():
+	f3.write(i+str(j)+"\n")
+total_dist = 0
+total_time = 0
+def dfs(graph,start,goal):
+	q = Queue.PriorityQueue()
+	q.put((-1,start))
+	visited = []
+	parent_dict = {}
+	path = []
+	j = -1
+	while True:
+		next = q.get()
+		if next[1] == goal:
+			print parent_dict
+			path = find_parent(parent_dict,goal,start,[goal])
+			return path
+		else:
+			tmp = []
+			for i in graph[next[1]]:
+				if i[0] not in set(visited):
+					tmp.append(i)
+			tmp.sort(key = lambda x:x[1])
+			for i in tmp:
+				k=0
+				q.put((j+k,i[0]))
+				parent_dict[i[0]] = next[1]
+				#parent_dict[i[0]] = next[1]
+				visited.append(i[0])					#q.put((j+k,i[0]))
+				#visited.append(i[0])
+				k-=1
+		j -= 1
+		#print visited
+		if q.empty():
+			break
+	if goal not in path:
+		print 'Not found'
+		return None
+'''
+def dfs(graph, start, goal, path=None):
+	print path
+	if path is None:
+		path = [start]
+	if start == goal:
+		return path
+	for next in graph[start]:
+		print "Next:"+next[0]
+		if next not in path:
+			dfs(graph, next[0], goal, path.append(next[0]))
+'''
+def find_parent(parent_dict,child,parent_target,path):
+	if parent_dict[child] == parent_target:
+		path.append(parent_target)
+		return path
+	else:
+		path.append(parent_dict[child])
+		return find_parent(parent_dict,parent_dict[child],parent_target,path)
+
+
+def bfs(graph,start,goal):
+	q = Queue.PriorityQueue()
+	q.put((-1,start))
+	visited = []
+	path = []
+	parent_dict = {}
+	j = 0
+	while not q.empty():
+		next = q.get()
+		#print next
+		if next[1] == goal:
+			print parent_dict
+			path = find_parent(parent_dict,goal,start,[goal])
+			return path
+		else:
+			k=j
+			tmp = []
+			for i in graph[next[1]]:
+		#		print i
+				if i[0] not in set(visited):
+					tmp.append(i)
+			tmp.sort(key = lambda x:x[1])
+			for i in tmp:
+				q.put((k,i[0]))
+				parent_dict[i[0]] = next[1]
+				visited.append(i[0])
+		j+=1
+	#print visited
+
+	if goal not in path:
+		print 'Not found'
+		return None
+
+'''
+if routing_algo == 'bfs':
+	path_bfs = bfs(seg_dict,start_city,end_city)
+elif routing_algo == 'dfs'
+	path_dfs = dfs(seg_dict,start_city,end_city)	
+'''
+print 'DFS'
+path_dfs = dfs(seg_dict,'Bloomington,_Indiana','Indianapolis,_Indiana')
+path_dfs.reverse()
+print path_dfs
+#path_dfs = dfs(seg_dict,'Eastman,_Georgia','Dublin,_Georgia')
+print 'BFS'
+path_bfs = bfs(seg_dict,'Bloomington,_Indiana','Indianapolis,_Indiana')
+#path_bfs = bfs(seg_dict,'Eastman,_Georgia','Dublin,_Georgia')
+
+#dfs(seg_dict,'Eastman,_Georgia','Eastman,_Georgia')
+path_bfs.reverse()
+print path_bfs
+
+
+f1.close()
+f2.close()
+f3.close()
+f4.close()
